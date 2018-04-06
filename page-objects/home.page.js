b377721429200b568fdadf2387381b4bd1636922
Xin Wang <xin@ECS0671.lan>
2018-04-06 08:13:49 +0100

    Add test for contact page

--- page-objects/home.page.js
+++ page-objects/home.page.js
@@ -1,18 +1,19 @@
 class HomePage {
-    // get appMonitoring () { return $('li.active-branch> ul.hs-menu-children-wrapper > li:nth-child(2)'); }
     get logo () { return $('.fusion-standard-logo'); }
     get mainMenu () { return $('.fusion-main-menu'); }
     get menuItems () { return browser.elements('#menu-qaworks-menu-general'); }
     get services () { return $('#menu-item-18924'); }
     get testAutomation() { return $('a[href="//www.qaworks.com/test-automation/"]');}
+    // get article() { return $('#post-25944')}
+    // get readArticle() { return $('a[href="//www.qaworks.com/articles/qaworks-acquired-by-ecs-digital/"]');}
 
     navigateToTestAutomation () {
         this.services.click();
         this.testAutomation.waitForVisible();
         this.testAutomation.click();
     }
+
+
 }
 
 export default new HomePage();
-
-//class, id and href(a)
--- specs/contact.spec.js
+++ specs/contact.spec.js
@@ -1,14 +1,14 @@
-// import ContactPage from '../page-objects/contact.page';
-//
-// describe('ECS Digital contact page', () => {
-//     before('Navigate to contact page', () => {
-//         browser.url('/contact');
-//     });
-//
-//     it.only('should show error message when required fields are not filled in', () => {
-//         //ContactPage.firstNameField.setValue('Marie');
-//         ContactPage.submitBtn.click();
-//         ContactPage.errorMsgs.waitForVisible();
-//         expect(ContactPage.errorMsgs.isVisible()).to.be.true;
-//     })
-// });
+import ContactPage from '../page-objects/contact.page';
+
+describe('QAWorks contact page', () => {
+    before('Navigate to contact page', () => {
+        browser.url('/contact-us/');
+    });
+
+    it.only('should show error message when required fields are not filled in', () => {
+        browser.setValue(ContactPage.nameField, 'Xin')
+        ContactPage.sendBtn.click();
+        ContactPage.errorMsgs.waitForVisible();
+        expect(ContactPage.errorMsgs.isVisible()).to.be.true;
+    })
+});
